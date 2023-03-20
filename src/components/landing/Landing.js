41370bb3b3fa546781412a9da9752a03d8f4f6e1
Maria <araujomaria84@gmail.com>
2023-03-20 12:03:32 +0100

    Add SCSS landing

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,10 +1,78 @@
 import { Link } from "react-router-dom";
+import Card from "../main/Card";
+// import "../../styles/layout/preview.scss";
+import "../../styles/layout/card.scss";
+
 
 function Landing () {
+    const obj1 = {
+        name: 'Nombre' ,
+        slogan: 'Prueba',
+        repo: 'Prueba',
+        demo: 'Prueba',
+        technologies: 'Prueba',
+        desc: 'Prueba',
+        autor: 'Prueba',
+        job: 'Prueba',
+        image: 'Prueba',
+        photo: 'Prueba',
+    }
+    const obj2 = {
+        name: 'Nombre' ,
+        slogan: 'Prueba',
+        repo: 'Prueba',
+        demo: 'Prueba',
+        technologies: 'Prueba',
+        desc: 'Prueba',
+        autor: 'Prueba',
+        job: 'Prueba',
+        image: 'Prueba',
+        photo: 'Prueba',
+    }
+    const obj3 = {
+        name: 'Nombre' ,
+        slogan: 'Prueba',
+        repo: 'Prueba',
+        demo: 'Prueba',
+        technologies: 'Prueba',
+        desc: 'Prueba',
+        autor: 'Prueba',
+        job: 'Prueba',
+        image: 'Prueba',
+        photo: 'Prueba',
+    }
+    const obj4 = {
+        name: 'Nombre' ,
+        slogan: 'Prueba',
+        repo: 'Prueba',
+        demo: 'Prueba',
+        technologies: 'Prueba',
+        desc: 'Prueba',
+        autor: 'Prueba',
+        job: 'Prueba',
+        image: 'Prueba',
+        photo: 'Prueba',
+    }
     return (
         <>
-            <p>Soy la Landing</p>
-            <Link to='/create-card'>Crear tarjeta</Link>
+            <section className="landing">
+                <article >
+                    <h2 className="title">Proyectos Molones</h2>
+                    <h3 className="subtitle">Escaparate en línea para recoger ideas a través de la tecnología</h3>
+
+                    <div className="section-btn">
+                    <Link className="buttons-img" to='/create-card'>Nuevo proyecto</Link>
+                    <Link className="buttons-img" to='/'>Ver proyectos</Link>
+                    </div>
+                </article>
+
+            <div className="landing-card">
+                <Card data={obj1}></Card>
+                <Card data={obj2}></Card>
+                <Card data={obj3}></Card>
+                <Card data={obj4}></Card>
+            </div>
+            </section>
         </>
     )
 }
--- src/components/main/Preview.js
+++ src/components/main/Preview.js
@@ -1,38 +1,12 @@
 import cover from "../../images/cover.jpeg";
-import user from "../../images/user.jpeg";
+import Card from "./Card";
 
 function Preview({ data }) {
-    console.log(data);
     return (
         <section className='preview'>
             <img className='image' src={data.image ? data.image : cover} alt='' />
+<Card data={data}></Card>
 
-            <section className='autor'>
-                <section className='info-project'>
-                    <p className='subtitle'>Personal Project Card</p>
-                    <hr className='line' />
-
-                    <h2 className='title-project'>{data.name || "Elegant workspace"}</h2>
-                    <p className='slogan'>{data.slogan || "Diseños Exclusivos"}</p>
-                    <p className='desc'>
-                        {data.desc ||
-                            "Lorem, ipsum dolor sit amet consectetur adipisicing elit Libero, delectus? Voluptates at hic aliquam porro ad suscip harum laboriosam saepe earum doloribus aperiam, ullam culpa accusantium placeat odit corrupti ipsum!"}
-                    </p>
-                    <section className='technologies'>
-                        <p className='text'>{data.technologies || "React JS, MongoDB"}</p>
-                        <div className='icons'>
-                            <a href={data.demo} rel='noreferrer' target='_blank'><i className='fas fa-globe icon'></i></a>
-                            <a href={data.repo} rel='noreferrer' target='_blank'><i className='fab fa-github icon'></i></a>
-                        </div>
-                    </section>
-                </section>
-
-                <section className='info-autor'>
-                    <img className='image-autor' src={data.photo ? data.photo : user} alt='' />
-                    <p className='job'>{data.job || "Full Stack Developer"}</p>
-                    <p className='name'>{data.autor || "Emmelie Björklund"}</p>
-                </section>
-            </section>
         </section>
     );
 }
